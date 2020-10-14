e30b38e758355948e25b3098a7827284382556c2
jsol3jx <jstephen@outlook.com>
2020-10-14 13:41:59 -0400

    Added documentation

--- Azure MVDAutomation/FindSessionHost.ps1
+++ Azure MVDAutomation/FindSessionHost.ps1
@@ -1,7 +1,10 @@
-#Created By: John Stephen
+#Created by: John Stephen
+#Created on: 9/32/2020
+#Written for: Azure Windows Virtual Desktop Fall 2019 (classic)
+#Purpose: When provided a user UPN(email address) it will take that and search the MVD environment to find what session host(s) they are assigned.
 
 
-
+#Pass the variables through before script runs.
 [CmdletBinding()]
      Param
      (
@@ -10,9 +13,10 @@
                 
      )
 
+#Connect to Windows Virtual Desktop with your ansys admin account
 Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com" | Out-Null
 
-
+#Function that does the actual work to find all assigned sessionhosts.
 function FindSessionName {
     param (
         [Parameter(Mandatory=$true)]
@@ -32,6 +36,7 @@ function FindSessionName {
 
 }
 
+#Sets the function output to a variable and outputs the session hosts assigned to the console.
 $SessionNames = FindSessionName -upn $upn
 $SessionNames = $SessionNames | Where-Object {$_ -ne $null}
 
--- Azure MVDAutomation/MVDCustomRDPProperties.ps1
+++ Azure MVDAutomation/MVDCustomRDPProperties.ps1
@@ -1,7 +1,9 @@
 #Created by: John Stephen
 #Date: 10/12/2020
-#This will add custom RDP properties to a hostpool.
+#Written for: Azure Windows Virtual Desktop Fall 2019 (classic)
+#Purpose: This will add custom RDP properties to a hostpool.
 
+#Pass the variables through before script runs.
 [CmdletBinding()]
      Param
      (
@@ -11,7 +13,9 @@
          [string] $hostpoolname
      )
 
+#Connect to Windows Virtual Desktop with your ansys admin account
 Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com" | Out-Null
 
+#sets the actual custom RDP properties
 $properties="drivestoredirect:s:;audiocapturemode:i:1;camerastoredirect:s:*"
 Set-RdsHostPool -TenantName $tenantname -Name $hostpoolname -CustomRdpProperty $properties
