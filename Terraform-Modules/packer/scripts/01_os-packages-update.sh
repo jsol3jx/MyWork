81afc7d545b41e2b4d8f48308873cef48ebfbd15
jsol3jx <jstephen@outlook.com>
2025-02-16 16:50:29 -0500

    updated comment in os packages update shell script.

--- Terraform-Modules/packer/scripts/01_os-packages-update.sh
+++ Terraform-Modules/packer/scripts/01_os-packages-update.sh
@@ -1,3 +1,5 @@
+#This script will install all available OS updates for Amazon Linux 2 w/ Docker.
+
 sudo amazon-linux-extras install kernel-5.15 -y
 sudo yum remove kernel.x86_64           5.10.118-111.515.amzn2           @amzn2extra-kernel-5.10 -y
 sudo /opt/elasticbeanstalk/bin/pkg-repo unlock
