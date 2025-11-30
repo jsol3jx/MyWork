4b84e7947aac8de003d3b08d13d672fe002efc3d
jsol3jx <jstephen@outlook.com>
2025-11-30 18:13:24 -0500

    updated data file for cloudfront tf module.

--- Terraform-Modules/terraform-aws-cloudfront-s3/data.tf
+++ Terraform-Modules/terraform-aws-cloudfront-s3/data.tf
@@ -1,18 +1,1 @@
 data "aws_canonical_user_id" "current" {}
-
-# Lookup existing certificate by domain name
-/*
-data "aws_acm_certificate" "acm_cert" {
-  domain   = "*.johnnyterraform.tech"
-  statuses = ["ISSUED"]
-
-  # optional: pick the most recent if multiple certs exist
-  most_recent = true
-}
-
-data "aws_route53_zone" "main" {
-  name         = var.domain_name
-  private_zone = false
-}
-
-*/
